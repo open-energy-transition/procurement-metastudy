76434712a2f5b065806f6d26fb64bd9070cc3b88
virio-andreyana <arsyan.virio@gmail.com>
2025-06-04 12:57:50 +0200

    improve seq_run userability

--- run/seq_run.py
+++ run/seq_run.py
@@ -12,27 +12,36 @@ def deep_update(original, updates):
         else:
             original[key] = value
 
-def select_scenario(scenarios):
-    print("Available scenarios:")
+def select_scenario(scenarios, name="scenarios"):
+    print(f"Available {name}:")
     for i, scenario in enumerate(scenarios, 1):
         print(f"{i}. {scenario}")
-    print("(Scenarios must include '--' in their name)")
+    if name == "scenarios":
+        print("(Scenarios must include '--' in their name)")
+    elif name == "baseline":
+        print("(Baselines must include 'baseline' in their name)")
+    print("(You can also select the list number)")
 
     while True:
-        answer = input("Select a scenario as base (or press Enter to cancel): ").strip()
+        answer = input("Select a scenario (or press Enter to cancel): ").strip()
         if not answer:
             return None
         if answer in scenarios:
             print(f"'{answer}' is selected.")
             return answer
+        elif answer.isdigit():
+            scenario_selected = scenarios[int(answer)-1]
+            print(f"'{scenario_selected}' is selected.")
+            return scenario_selected
         else:
             print(f"'{answer}' not found in the list. Please try again.")
 
-def select_country(df):
+def select_country():
+    df = pd.read_csv("run/name_location_country.csv")
     available_countries = set(df["country"].unique())
     main_countries = {"DE", "FR", "PL", "ES", "IE", "DK"}
 
-    print("\nSelect which country or group of countries to execute:")
+    print("Select which country or group of countries to execute:")
     print("  - main: DE, FR, PL, ES, IE, DK")
     print("  - all: all 34 countries individually")
     print("  - or enter country codes separated by commas (e.g., DE,FR,PL)")
@@ -61,6 +70,18 @@ def select_country(df):
                 print(f"Selected countries: {', '.join(sorted(selected))}")
                 return ",".join(sorted(selected)), df[df["country"].isin(selected)]
 
+def select_procedure():
+    while True:
+        answer = input("Do you want all countries in the same model [y/n]?: ").strip().lower()
+        if not answer:
+            return None
+        elif answer in ["y", "yes"]:
+            return "combined"
+        elif answer in ["n", "no"]:
+            return "sequential"
+
+        print(f"'{answer}' is not a valid option. Please try again.")
+
 def select_profile():
     while True:
         answer = input("Is this in a computer cluster [y/n]?: ").strip().lower()
@@ -76,6 +97,43 @@ def select_profile():
                 return f"-c{cpu}"
         print(f"'{answer}' is not a valid option. Please try again.")
 
+def duplicate_run_delete(scenario_name, selected_baseline, selected_profile):
+    # Prepare resources
+    new_folder = f"resources/{scenario_name}"
+    old_folder = f"resources/{selected_baseline}"
+    files_to_copy = ["costs_2030.csv", "networks/base_s_39___2030_brownfield.nc"]
+
+    os.makedirs(os.path.join(new_folder, "networks"), exist_ok=True)
+
+    for f in files_to_copy:
+        old_path = os.path.join(old_folder, f)
+        new_path = os.path.join(new_folder, f)
+        if os.path.isfile(old_path):
+            shutil.copy(old_path, new_path)
+            print(f"Copied {old_path} to {new_path}")
+        else:
+            print(f"WARNING: File not found: {old_path}")
+
+    # Run snakemake
+    run_cmd = f"snakemake {selected_profile} solve_sector_networks --configfile run/config.meta_temp.yaml --rerun-trigger mtime"
+    os.system(run_cmd + " --touch")
+    os.system(run_cmd)
+
+    # Clean up
+    if os.path.exists(new_folder):
+        shutil.rmtree(new_folder)
+        print(f"Deleted folder: {new_folder}")
+    else:
+        print(f"Folder does not exist (already removed?): {new_folder}")
+
+    temp_file = 'run/config.meta_temp.yaml'
+    
+    if os.path.exists(temp_file):
+        os.remove(temp_file)
+        print(f"Deleted file: {temp_file}")
+    else:
+        print(f"File does not exist: {temp_file}")
+
 # ---------------------- Main Script ----------------------
 
 def main():
@@ -83,22 +141,35 @@ def main():
     with open('config/scenarios.meta.yaml', 'r') as file:
         config_s = yaml.safe_load(file)
 
-    df = pd.read_csv("run/name_location_country.csv")
-
     # Extract scenarios with '--'
     scenario_list = [key for key in config_s if "--" in key]
+    baseline_list = [key for key in config_s if "baseline" in key]
 
     # User input
-    selected_scenario = select_scenario(scenario_list)
+    selected_scenario = select_scenario(scenario_list, name="scenario")
     if not selected_scenario:
         print("\nOperation cancelled by user.")
         return
+    
+    print("\n=================================================================")
+    selected_baseline = select_scenario(baseline_list, name="baseline")
+    if not selected_baseline:
+        print("\nOperation cancelled by user.")
+        return
 
-    selected_country, df = select_country(df)
+    print("\n=================================================================")
+    selected_country, df = select_country()
     if not selected_country:
         print("\nOperation cancelled by user.")
         return
 
+    print("\n=================================================================")    
+    selected_procedure = select_procedure()
+    if not selected_procedure:
+        print("\nOperation cancelled by user.")
+        return
+
+    print("\n=================================================================")
     selected_profile = select_profile()
     if not selected_profile:
         print("\nOperation cancelled by user.")
@@ -106,20 +177,21 @@ def main():
 
     print("\nFinal selection:")
     print(f"Scenario: {selected_scenario}")
+    print(f"Baseline: {selected_baseline}")
     print(f"Country group: {selected_country}")
+    print(f"Country inclusion: {selected_procedure}")
     print(f"Profile: {selected_profile}")
 
     # Iterate runs
-    for i, row in df.iterrows():
-        # Load base config
+    if selected_procedure == "combined":
         with open('config/config.meta.yaml', 'r') as file:
-            config = yaml.safe_load(file)
+                config = yaml.safe_load(file)
 
         deep_update(config, config_s[selected_scenario])
 
         # Modify config per country
-        country = row["country"]
-        scenario_name = selected_scenario.replace("--", f"-{country}-")
+        scenario_name = selected_scenario.replace("--", f"-{selected_country}-")
+        ci_list = df.set_index("name")[["location"]].to_dict('index')
 
         config_update = {
             "run": {
@@ -127,9 +199,7 @@ def main():
                 "scenarios": {"enable": False}
             },
             "procurement": {
-                "ci": {
-                    row["name"]: {"location": row["location"]}
-                }
+                "ci": ci_list
             }
         }
 
@@ -139,41 +209,40 @@ def main():
         with open('run/config.meta_temp.yaml', 'w') as file:
             yaml.safe_dump(config, file, default_flow_style=False)
 
-        # Prepare resources
-        new_folder = f"resources/{scenario_name}"
-        old_folder = "resources/baseline-3H" if "3H" in scenario_name else "resources/baseline-1H"
-        files_to_copy = ["costs_2030.csv", "networks/base_s_39___2030_brownfield.nc"]
+        duplicate_run_delete(scenario_name, selected_baseline, selected_profile)
 
-        os.makedirs(os.path.join(new_folder, "networks"), exist_ok=True)
 
-        for f in files_to_copy:
-            old_path = os.path.join(old_folder, f)
-            new_path = os.path.join(new_folder, f)
-            if os.path.isfile(old_path):
-                shutil.copy(old_path, new_path)
-                print(f"Copied {old_path} to {new_path}")
-            else:
-                print(f"WARNING: File not found: {old_path}")
+    elif selected_procedure == "sequential":
+        for i, row in df.iterrows():
+            # Load base config
+            with open('config/config.meta.yaml', 'r') as file:
+                config = yaml.safe_load(file)
 
-        # Run snakemake
-        run_cmd = f"snakemake {selected_profile} solve_sector_networks --configfile run/config.meta_temp.yaml --rerun-trigger mtime"
-        os.system(run_cmd + " --touch")
-        os.system(run_cmd)
+            deep_update(config, config_s[selected_scenario])
 
-        # Clean up
-        if os.path.exists(new_folder):
-            shutil.rmtree(new_folder)
-            print(f"Deleted folder: {new_folder}")
-        else:
-            print(f"Folder does not exist (already removed?): {new_folder}")
+            # Modify config per country
+            country = row["country"]
+            scenario_name = selected_scenario.replace("--", f"-{country}-")
 
-        temp_file = 'run/config.meta_temp.yaml'
-        
-        if os.path.exists(temp_file):
-            os.remove(temp_file)
-            print(f"Deleted file: {temp_file}")
-        else:
-            print(f"File does not exist: {temp_file}")
+            config_update = {
+                "run": {
+                    "name": scenario_name,
+                    "scenarios": {"enable": False}
+                },
+                "procurement": {
+                    "ci": {
+                        row["name"]: {"location": row["location"]}
+                    }
+                }
+            }
+
+            deep_update(config, config_update)
+
+            # Write temp config
+            with open('run/config.meta_temp.yaml', 'w') as file:
+                yaml.safe_dump(config, file, default_flow_style=False)
+
+            duplicate_run_delete(scenario_name, selected_baseline, selected_profile)
 
 if __name__ == "__main__":
     main()
